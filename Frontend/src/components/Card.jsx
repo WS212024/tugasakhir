295f23c074527ae1df1037efb059d72e91570b42
WS212024 <weathersta123@gmail.com>
2024-08-06 21:36:20 +0800

    upd

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -59,6 +59,7 @@ const Card = () => {
 }, []);
 
 useEffect(() => {
+  if (hujan && ldr) {
     if (ldr >= 900) {
       setWeatherImage(Gelap); // Set image for very dark weather
     } else if (ldr >= 500) {
@@ -66,7 +67,7 @@ useEffect(() => {
     } else {
       setWeatherImage(Terang); // Set default image
     }
-
+  }
 }, [hujan, ldr]); // Added hujan and ldr to dependencies
 
   return (
